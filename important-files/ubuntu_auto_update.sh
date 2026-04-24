5de1126cf87ef4056d78cb5440e51c8740ada08c
Krushnakant Sadiya <34934876+sadiakant@users.noreply.github.com>
2026-04-24 15:48:07 +0530

    Add error checking and fixing for dpkg in script

--- important-files/ubuntu_auto_update.sh
+++ important-files/ubuntu_auto_update.sh
@@ -8,6 +8,12 @@ echo -e "\n=== Ubuntu System Update Started: $(date) ===" >> "$LOG"
 echo "→ Running apt-get update..." | tee -a "$LOG"
 apt-get update 2>&1 | tee -a "$LOG"
 
+echo -e "\n→ Checking dpkg Errors..." | tee -a "$LOG"
+dpkg --configure -a 2>&1 | tee -a "$LOG"
+
+echo -e "\n→ Fixing Broken dpkg..." | tee -a "$LOG"
+apt --fix-broken install -y 2>&1 | tee -a "$LOG"
+
 echo -e "\n→ Running apt-get upgrade..." | tee -a "$LOG"
 apt-get upgrade -y 2>&1 | tee -a "$LOG"
 
